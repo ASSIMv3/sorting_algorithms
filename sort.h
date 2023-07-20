5e9f07334984910aca76db8f9e0678da7ad056b5
ASSIMv3 <hassanassim01@gmail.com>
2023-07-20 04:58:33 -0700

    fixing sort.h file

--- sort.h
+++ sort.h
@@ -4,10 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void bubble_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -17,9 +13,13 @@ void print_list(const listint_t *list);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
+
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 #endif /* MAIN_H */
