ff3d889cb6ff4ff4db38ed07b02df5a39fcb6e2d
Sana Assim <sanaassim1@gmail.com>
2023-07-20 06:43:57 -0700

    Add quick sort functions

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+int lomuto_partition(int *array, size_t size, int low, int high);
+void quicksort(int *array, size_t size, int low, int high);
 void quick_sort(int *array, size_t size);
 
 #endif /* MAIN_H */
