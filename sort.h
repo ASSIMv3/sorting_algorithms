c89f7f7f89ad040271ff568e291558ee0781700f
Sana Assim <sanaassim1@gmail.com>
2023-07-21 04:48:19 -0700

    Add counting_sort function prototype

--- sort.h
+++ sort.h
@@ -29,5 +29,7 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *a, listint_t *b);
 void cocktail_sort_list(listint_t **list);
+int find_max(int *array, int size);
+void counting_sort(int *array, size_t size);
 
 #endif /* MAIN_H */
